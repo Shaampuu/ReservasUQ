d06f9d1fb0500443c325d8d8f0c8353ff1541d0f
Juan Corrales <juanc.corralesc@uqvirtual.edu.co>
2024-11-05 18:35:06 -0500

    actualización login

--- src/main/java/co/edu/uniquindio/reservasuq/RESERVA.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.reservasuq;
-
-public class RESERVA {
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module co.edu.uniquindio.reservasuq {
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
     requires org.simplejavamail;
     requires org.simplejavamail.core;
+    requires static lombok;
 
 
     opens co.edu.uniquindio.reservasuq to javafx.fxml;
-    exports co.edu.uniquindio.rese  rvasuq;
     exports co.edu.uniquindio.reservasuq.App;
     opens co.edu.uniquindio.reservasuq.App to javafx.fxml;
 }
