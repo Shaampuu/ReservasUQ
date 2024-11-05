b972dfc870b4ad0e61f1dc637d1ccfb4c27e2d5c
Juan Corrales <juanc.corralesc@uqvirtual.edu.co>
2024-11-05 15:58:36 -0500

    se suben fxml

--- src/main/java/co/edu/uniquindio/reservasuq/Utils/EnvioEmail.java
+++ src/main/java/co/edu/uniquindio/reservasuq/Utils/EnvioEmail.java
@@ -1,4 +1,36 @@
 package co.edu.uniquindio.reservasuq.Utils;
 
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+
 public class EnvioEmail {
+
+
+    public static void enviarNotificacion(String destinatario, String asunto, String mensaje) {
+
+
+        Email email = EmailBuilder.startingBlank()
+                .from("juanc.corralesc@uqvirtual.edu.co")
+                .to(destinatario)
+                .withSubject(asunto)
+                .withPlainText(mensaje)
+                .buildEmail();
+
+
+        try (Mailer mailer = MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 587, "juanc.corralesc@uqvirtual.edu.co", "hngm knvf pvud juoi\n")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .withDebugLogging(true)
+                .buildMailer()) {
+
+
+            mailer.sendMail(email);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
